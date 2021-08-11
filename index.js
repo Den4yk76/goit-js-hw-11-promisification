f704af344a6bc75d667bad8cf480df1d05e6d95f
Denys Nazarenko <denysnazarenko23@gmail.com>
2021-08-11 17:03:42 +0300

    Update index.js
    HW-11 Task 3.2 is Done!

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+///--------------------------------------------------------------///
+///-------------------------Task 3.1-----------------------------///
+///--------------------------------------------------------------///
 const delay = ms => {
     return new Promise(resolve => {
         setTimeout(() => {
@@ -6,9 +9,38 @@ const delay = ms => {
     });
 };
 
-const logger = time => console.log(`Resolved after ${time}ms`);
+const logger1 = time => console.log(`Resolved after ${time}ms`);
 
 // Вызовы функции для проверки
-delay(2000).then(logger); // Resolved after 2000ms
-delay(1000).then(logger); // Resolved after 1000ms
-delay(1500).then(logger); // Resolved after 1500ms
+delay(2000).then(logger1); // Resolved after 2000ms
+delay(1000).then(logger1); // Resolved after 1000ms
+delay(1500).then(logger1); // Resolved after 1500ms
+///--------------------------------------------------------------///
+
+///--------------------------------------------------------------///
+///-------------------------Task 3.2-----------------------------///
+///--------------------------------------------------------------///
+const users = [
+    { name: 'Mango', active: true },
+    { name: 'Poly', active: false },
+    { name: 'Ajax', active: true },
+    { name: 'Lux', active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+    return new Promise(resolve => {
+        resolve(
+            allUsers.map(user =>
+                user.name === userName
+                    ? { ...user, active: !user.active }
+                    : user,
+            ),
+        );
+    });
+};
+
+const logger2 = updatedUsers => console.table(updatedUsers);
+
+toggleUserState(users, 'Mango').then(logger2);
+toggleUserState(users, 'Lux').then(logger2);
+///--------------------------------------------------------------///
